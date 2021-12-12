cef0b21a9fa0393d21319b4b61b292c7792dd96b
Brady <brady.drew.logan@gmail.com>
2021-12-12 13:20:51 -0500

    tournament bots no longer battle themselves

--- examples/BotBattleRoyale.py
+++ examples/BotBattleRoyale.py
@@ -43,7 +43,7 @@ async def main():
     ))
     
     for i in range(len(players.keys())):
-        for j in range(i, len(players.keys())):
+        for j in range(i + 1, len(players.keys())):
             await players[list(players.keys())[i]].battle_against(players[list(players.keys())[j]], n_battles=1000)
             print("battle finished")
 
